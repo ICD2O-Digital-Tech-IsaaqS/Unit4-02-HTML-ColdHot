4873e8d7984f44de776a6143b5ab143f95dd7fb9
Isaaq Simon <IsaaqS>
2025-04-28 11:47:37 +0000

    Temperature

--- js/script.js
+++ js/script.js
@@ -1,5 +1,30 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2025 Isaaq Simon All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Isaaq Simon
+// Created on: April 2025
 // This file contains the JS functions for index.html
+// Function to check the temperature input
+function checkTemperature() {
+    // Get the value from the input and convert to a number
+    const celsius = parseFloat(document.getElementById('celsiusInput').value);
+
+    // Get the element where we will display the result
+    const result = document.getElementById('result');
+
+    // Check if input is a valid number
+    if (isNaN(celsius)) {
+        result.textContent = "Please enter a valid number!";
+        result.style.color = "orange";
+    }
+    else {
+        // If temperature is less than 15
+        if (celsius < 15) {
+            result.textContent = "It is cold outside!";
+            result.style.color = "blue"; // Cold = blue
+        }
+        else {
+            result.textContent = "It is hot outside!";
+            result.style.color = "red"; // Hot = red
+        }
+    }
+}
